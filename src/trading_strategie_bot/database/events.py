aeb49172d7b15fb37f7d04b71154271d412d92c6
Oliinant <niilo.abbett@gmail.com>
2025-08-24 00:52:19 +0200

    Refactor event functions

--- src/trading_strategie_bot/database/events.py
+++ src/trading_strategie_bot/database/events.py
@@ -1,65 +1,85 @@
 from sqlalchemy import text
 from decimal import Decimal
 
-def fetch_model_obj_by_id(connection, id):
+
+def fetch_backtest_obj_by_id(connection, id):
     obj = connection.execute(
-        text("SELECT * FROM tests WHERE id=:id"), 
+        text("SELECT * FROM backtests WHERE id=:id"), 
         {"id": id}
-    ).fetchone()
+    ).mappings().fetchone()
     return None if obj is None else obj
 
 def fetch_holding_by_ticker(connection, ticker):
     holding = connection.execute(
-        text("SELECT * FROM tests WHERE ticker=:ticker"), 
+        text("SELECT * FROM holdings WHERE ticker=:ticker"), 
         {"ticker": ticker}
-    ).fetchone()
+    ).mappings().fetchone()
     return None if holding is None else holding
 
-def require_balance(test):
-    balance = test[2]
+def require_balance(balance):
     if balance is None:
-        raise ValueError("Error: Trade must be linked to a valid test balance")
-    return balance
+        raise ValueError("Error: Trade must be linked to a valid backtest balance")
 
 def calc_total(shares, price):
     return shares * price
 
-def check_balance_for_entry(target):
-    balance = target._test[2]
-    if balance < target._total_cost:
-        raise ValueError("Error: Insufficent balance")
+def has_required_items_for_trade(value, cost, error_descriptor):
+    if value < cost:
+        raise ValueError(f"Error: Insufficent {error_descriptor}")
 
+def calc_new_balance_shares(old_value, value_to_add, sign):
+    return Decimal(old_value) + (Decimal(value_to_add) * sign)
 
-def update_balance(connection, target, total, sign=1):
-    test_id = target._test[0]
-    test_balance = target.test[2]
-    new_balance = Decimal(test_balance) + (Decimal(total) * sign)
+def update_balance_holding_by_id(connection, table, value_to_update, new_value, id):
     connection.execute(
-        text("UPDATE tests SET balance=:balance WHERE id=:id"),
-        {"balance": new_balance, "id": test_id}
+        text(f"UPDATE {table} SET {value_to_update}=:{value_to_update} WHERE id=:id"),
+        {value_to_update: new_value, "id": id}
     )
 
-def update_holdings(connection, target, holding, sign=1):
-    test_id = target._test[0]
-    holding_id = holding[0]
-    holding_shares = holding[2]
-    if holding is None:
+def create_new_holding(connection, backtests_id, ticker, bought_shares):
         connection.execute(
-            text("INSERT INTO holdings (test_id, ticker, shares) VALUES (:test_id, :ticker, :shares)"),
-            {"test_id": test_id, "ticker": target.ticker, "shares": target.shares}
+            text("INSERT INTO holdings (backtests_id, ticker, shares) VALUES (:backtests_id, :ticker, :shares)"),
+            {"backtests_id": backtests_id, "ticker": ticker, "shares": bought_shares}
         )  
-    else:
-        new_share_amount = Decimal(holding_shares) + (Decimal(target.quantity) * sign)
-        connection.execute(
-            text("UPDATE holdings SET shares=:shares WHERE id=:id"),
-            {"shares": new_share_amount, "id": holding_id}
-        )
 
-def holding_exists(holding):
+def check_holding_exists(holding):
     if holding is None:
-        raise ValueError("Error: No holdings available for sale")
+        return False
+    else:
+        return True
 
-def check_holding_for_exit(target):
-    shares = target._holding[2]
-    if shares < target.shares:
-        raise ValueError("Error: cannot sell more shares then owned")
+
+def prepare_backtest_for_buy_in(connection, target):
+    target._backtest = fetch_backtest_obj_by_id(connection, target.backtest_id)
+    require_balance(target._backtest)
+    
+    total_cost = calc_total(target.shares, target.entry_price)
+    has_required_items_for_trade(target.backtest["balance"], total_cost, "balance")
+    
+    return calc_new_balance_shares(target._backtest["balance"], total_cost, -1)
+
+def prepare_holding_for_buy_in(connection, target):
+    target._holding = fetch_holding_by_ticker(connection, target.ticker)
+    
+    if check_holding_exists(target._holding):
+        return calc_new_balance_shares(target._holding["shares"], target.shares, 1)
+    return None
+
+
+def prepare_backtest_for_exit(connection, target):
+    target._backtest = fetch_backtest_obj_by_id(connection, target.backtest_id)
+    require_balance(target._backtest["balance"])
+    
+    total_profit = calc_total(target.shares, target.exit_price)
+    
+    return calc_new_balance_shares(target._backtest["balance"], total_profit, 1)
+
+def prepare_holding_for_exit(connection, target):
+    target._holding = fetch_holding_by_ticker(connection, target.ticker)
+    if check_holding_exists(target._holding):
+        raise ValueError("Error: Share does not exist on backtest")
+    
+    has_required_items_for_trade(target.holding["shares"], target.shares, "shares")
+    
+    return calc_new_balance_shares(target._holding["shares"], target.shares, -1)
+    
